1e9551d8996e1e7bb1efbda6c6d920e68875a2be
vaanskii <giorgivanadze03@gmail.com>
2024-02-18 04:15:52 +0400

    commit folders which recommended from vscode

--- v_backend/account/forms.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.forms import UserCreationForm
-
-from .models import User
-
-class SignupForm(UserCreationForm):
-    class Meta:
-        model = User
-        fields = ('email', 'username', 'password1', 'password2')
