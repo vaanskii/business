a98d9e22df8440938cbfbe3b5b76429129fd533a
unknown <giorgivanadze03@gmail.com>
2024-02-26 08:31:56 -0800

    frontend is builded

--- v_frontend/tailwind.config.js
+++ v_frontend/tailwind.config.js
@@ -1,11 +1,10 @@
 module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-   darkMode: false,
-   theme: {
-     extend: {},
-   },
-   variants: {
-     extend: {},
-   },
-   plugins: [],
- }
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
